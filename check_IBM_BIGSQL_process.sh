ad54d580720d34b83140b184bda30c40abd803e1
fwhadoopadmin <77496212+fwhadoopadmin@users.noreply.github.com>
2021-01-15 12:04:58 -0500

    Create check_IBM_BIGSQL_process.sh

---
+++ check_IBM_BIGSQL_process.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#############################################
+fgGreen=$(tput setaf 2)     # green
+fgRed=$(tput setaf 1)       # red
+fgYellow=$(tput setaf 3)    # yellow
+blue=$(tput setaf 4)        #blue
+normal=$(tput sgr0)         #normal
+#------------------------------------------------------------
+
+SCR=`basename $0`
+LINEOUT="/export/home/ambari/fwWorkstation/ansibleWorkstation/normalscripts/systemhealth/qa.lineout"
+#--------------------------------------------------------------------
+result="$(ps -ef|grep -i mapreduce  |grep -v "grep" |grep -i jar | awk '{print $2}' |wc -l)"
+
+echo $result 2>&1 >/dev/null;
+if [ $result -le 0 ]; then
+    echo "SWAP WARNING" 2>&1 >/dev/null;
+    status="WARN"
+    msg1="BIGSQL Warning!!: BIGSQL is either Down  or not in this machine please validate"
+    msg="$(printf "%40s" "${fgYellow} $msg1${normal}")"
+    REPORT="$( $LINEOUT "$status $SCR $msg" | sed '/^\s*$/d')"
+    printf "%s\n" "$REPORT"
+
+else
+    echo "Swap condition is Green" 2>&1 >/dev/null;
+    status="OK"
+    msg1="BIGSQL Warning!!: BIGSQL is either Down  or not in this machine please validate"
+    msg="$(printf "%40s" "${fgGreen} $msg1${normal}")"
+    REPORT="$( $LINEOUT "$status $SCR $msg" | sed '/^\s*$/d')"
+    printf "%s\n" "$REPORT"
+fi
